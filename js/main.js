45bc6c6dac707e9fa693570e2ec25adb48e601bb
arazon005 <arazon005@gmail.com>
2024-11-11 20:38:31 +0000

    added input event listener to photo url to add a photo preview (#5)

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
+'use strict';
 const $imageInput = document.querySelector('.photo-url');
 const $image = document.querySelector('img');
 if (!$imageInput) throw new Error('$imageInput query failed');
 if (!$image) throw new Error('$image query failed');
-
 $imageInput.addEventListener('input', (event) => {
-  const input = event.target as HTMLInputElement;
+  const input = event.target;
   console.log(input.value);
   if (input.value === '') {
     $image.setAttribute('src', '/images/placeholder-image-square.jpg');
